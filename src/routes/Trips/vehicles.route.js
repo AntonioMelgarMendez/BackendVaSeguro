56cd5bbf910a30b8089ce809b1344fee6f9e02c6
Daniel <00009123@uca.edu.sv>
2025-06-15 12:20:18 -0600

    Adding more auth security in vehicles

--- src/routes/Trips/vehicles.route.js
+++ src/routes/Trips/vehicles.route.js
@@ -3,10 +3,10 @@ const router = express.Router();
 const controller = require('../../controllers/Trips/vehicles.controller');
 const upload = require('../../middlewares/upload');
 
-router.get('/', controller.getAll);
-router.get('/:id', controller.getById);
+router.get('/',authenticateToken, authorizeRoles('admin'), controller.getAll);
+router.get('/:id',authenticateToken, authorizeRoles('admin','user','driver'), controller.getById);
 router.post('/', upload.single('car_pic'), controller.create);
-router.put('/:id', upload.single('car_pic'), controller.update); 
-router.delete('/:id', controller.remove);
+router.put('/:id',authenticateToken, upload.single('car_pic'), controller.update); 
+router.delete('/:id', authenticateToken,controller.remove);
 
 module.exports = router;
