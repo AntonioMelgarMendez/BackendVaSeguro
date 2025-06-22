cd8ab33c76217c5d4f161b70045563511e10efa7
Daniel <00009123@uca.edu.sv>
2025-06-22 16:59:06 -0600

    fixing miss click route

--- src/routes/Trips/vehicles.route.js
+++ src/routes/Trips/vehicles.route.js
@@ -5,8 +5,8 @@ const upload = require('../../middlewares/upload');
 const { authenticateToken, authorizeRoles } = require('../../middlewares/authentication');
 
 router.get('/',authenticateToken, authorizeRoles('admin'), controller.getAll);
-router.get('/:id',authenticateToken, authorizeRoles('admin','user','driver'), controller.getById);
-router.get('/:driver_id',authenticateToken,authenticateToken('admin','user','driver'),controller.getByDriverId)
+router.get('/driver/:driver_id', authenticateToken, authorizeRoles('admin','user','driver'), controller.getByDriverId);
+router.get('/:id', authenticateToken, authorizeRoles('admin','user','driver'), controller.getById);
 router.post('/', upload.single('car_pic'), controller.create);
 router.put('/:id',authenticateToken, upload.single('car_pic'), controller.update); 
 router.delete('/:id', authenticateToken,controller.remove);
