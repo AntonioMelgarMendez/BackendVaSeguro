1d5fc38f4f4800fd56761d2c0150d2d7aeabd931
Daniel <00009123@uca.edu.sv>
2025-07-01 17:50:38 -0600

    adding fix to find player bi onesignal_id

--- src/controllers/calls.controller.js
+++ src/controllers/calls.controller.js
@@ -3,7 +3,7 @@ const sendNotification = require('../utils/sendNotifications');
 
 async function createCall(req, res) {
     try {
-        console.log(re.body)
+     console.log(req.body)
       const call = await callsService.createCall(req.body);
   
       // Use the original callee_id from the request body
